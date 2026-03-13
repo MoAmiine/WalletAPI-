c1537a59c182ffd0b631fbefab61b591d77c4264
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 12:16:45 +0000

    created transaction model with relations and migrations

---
+++ database/migrations/2026_03_13_121431_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('wallet_id')->constrained()->cascadeOnDelete();
+            $table->string('type');
+            $table->decimal('amount', 15, 2);
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('receiver_wallet_id')->nullable();
+            $table->unsignedBigInteger('sender_wallet_id')->nullable();
+            $table->decimal('balance_after', 15, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};

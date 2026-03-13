283cebf8072838e78932f84f485f7911ad3d06ed
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 11:50:34 +0000

    Completed logout and user method to logout and show user profile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AuthController extends Controller
 {
@@ -28,5 +32,46 @@ class AuthController extends Controller
                 "token" => $token
             ]
         ], 201);
+    }
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                "message" => "Invalid credentials"
+            ], 401);
+        }
+        $token = $user->createToken("auth_token")->plainTextToken;
+
+        return response()->json([
+            "success" => true,
+            "message" => "Connexion réussie.",
+            "data" => [
+                "user" => $user,
+                "token" => $token
+            ]
+        ], 200);
+    }
+    public function logout(Request $request)
+    {
+
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Déconnexion réussie."
+        ], 200);
+    }
+    public function user(Request $request)
+    {
+
+        return response()->json([
+            "success" => true,
+            "message" => "Profil utilisateur récupéré.",
+            "data" => [
+                "user" => $request->user()
+            ]
+        ], 200);
     }
 }

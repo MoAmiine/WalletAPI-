5e4b011b0a66a2fa7b4e50f5e986b572a974438a
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 11:54:52 +0000

    added wallet relation to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function wallets()
+    {
+        return $this->hasMany(Wallet::class);
+    }
 }

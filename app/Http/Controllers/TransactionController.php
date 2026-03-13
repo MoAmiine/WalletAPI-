f6228cb7a5a0353419ea27d148e29f3d6e9affc1
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 12:25:26 +0000

    created transactionController and completed deposit method

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Wallet;
+use App\Models\Transaction;
+
+class TransactionController extends Controller
+{
+    public function deposit(Request $request, $id)
+    {
+        $wallet = Wallet::find($id);
+
+        if (!$wallet) {
+            return response()->json([
+                "success" => "false",
+                "message" => "Wallet introuvable."
+            ], 404);
+        }
+
+        $amount = $request->amount;
+        if ($amount <= 0) {
+            return response()->json([
+                "success" => false,
+                "message" => "Erreur de validation.",
+                "errors" => [
+                    "amount" => ["Le montant doit être supérieur à 0."]
+                ]
+            ], 422);
+        }
+        $wallet->balance += $amount;
+
+        $wallet->save();
+
+        $transaction = Transaction::create([
+            'wallet_id' => $wallet->id,
+            'type' => 'deposit',
+            'amount' => $amount,
+            'description' => $request->description,
+            'balance_after' => $wallet->balance
+        ]);
+        
+        return response()->json([
+            "success" => true,
+            "message" => "Dépôt effectué avec succès.",
+            "data" => [
+                "transaction" => $transaction,
+                "wallet" => [
+                    "id" => $wallet->id,
+                    "name" => $wallet->name,
+                    "currency" => $wallet->currency,
+                    "balance" => $wallet->balance
+                ]
+            ]
+        ]);
+    }
+}

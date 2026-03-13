f68f6b2d4327147c1260222ce55336536b5cc51a
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 15:25:42 +0000

    completed withdraw method in transaciton controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -40,7 +40,7 @@ class TransactionController extends Controller
             'description' => $request->description,
             'balance_after' => $wallet->balance
         ]);
-        
+
         return response()->json([
             "success" => true,
             "message" => "Dépôt effectué avec succès.",
@@ -55,4 +55,59 @@ class TransactionController extends Controller
             ]
         ]);
     }
+
+    public function withdraw(Request $request, $id)
+    {
+        $wallet = Wallet::find($id);
+
+        if (!$wallet) {
+            return response()->json([
+                "success" => false,
+                "message" => "Wallet introuvable."
+            ], 404);
+        }
+        $amount = $request->amount;
+
+        if ($amount <= 0) {
+            return response()->json([
+                "success" => false,
+                "message" => "Erreur de validation.",
+                "errors" => [
+                    "amount" => ["Le montant doit être supérieur à 0."]
+                ]
+            ], 422);
+        }
+
+        if ($wallet->balance < $amount) {
+            return response()->json([
+                "success" => false,
+                "message" => "Solde insuffisant. Solde actuel : {$wallet->balance} {$wallet->currency}."
+            ], 400);
+        }
+
+        $wallet->balance -= $amount;
+        $wallet->save();
+
+        $transaction = Transaction::create([
+            'wallet_id' => $wallet->id,
+            'type' => 'withdraw',
+            'amount' => $amount,
+            'description' => $request->description,
+            'balance_after' => $wallet->balance
+        ]);
+
+        return response()->json([
+        "success" => true,
+        "message" => "Retrait effectué avec succès.",
+        "data" => [
+            "transaction" => $transaction,
+            "wallet" => [
+                "id" => $wallet->id,
+                "name" => $wallet->name,
+                "currency" => $wallet->currency,
+                "balance" => $wallet->balance
+            ]
+        ]
+    ]);
+}
 }

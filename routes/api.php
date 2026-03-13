892be64775660e49c9b0fc9bf61062c3e5023861
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 11:51:08 +0000

    added routes for logout and userProfile

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TestController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',[AuthController::class,'login']);
 Route::apiResource('/test', TestController::class);
+
+Route::middleware('auth:sanctum')->group(function(){
+
+Route::post('/logout',[AuthController::class,'logout']);
+Route::get('/user',[AuthController::class,'user']);
+
+});
+

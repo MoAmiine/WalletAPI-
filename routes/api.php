e7014b95f170a8cda05f41bde1b4c13456d67c04
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-13 12:14:22 +0000

    added routes for walletcontroller

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\WalletController;
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::apiResource('/test', TestController::class);
 
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::post('/logout',[AuthController::class,'logout']);
-Route::get('/user',[AuthController::class,'user']);
-
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/wallets', [WalletController::class, 'store']);    
+    Route::get('/wallets', [WalletController::class, 'index']);
+    Route::get('/wallets/{id}', [WalletController::class, 'show']);
 });
-
